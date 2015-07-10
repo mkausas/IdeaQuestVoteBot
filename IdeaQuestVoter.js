18a961c202878215e87f2b56230e06a7d0d4a69f
mkausas <mkausas@gmail.com>
2015-07-10 13:06:27 -0700

    Initial commit; basic voting work with 5 second refresh; currently not smart when voting between two random ideas

--- IdeaQuestVoter.js
+++ IdeaQuestVoter.js
@@ -7,6 +7,8 @@ setInterval(
 
 function vote() {
 
+    console.log("Working...")
+
     // Titles
     var title1 = $('#ideaCard1 .ideaDetails .idea-title-container .title .dotdotdot h2').text();
     var title2 = $('#ideaCard2 .ideaDetails .idea-title-container .title .dotdotdot h2').text();
@@ -26,9 +28,32 @@ function vote() {
 
         // Not mine!
         else {
-            console.log("Not");
-            var button = $('#ideaCard2 .ideaDetails div .speuiSpriteButton a');
-            button.click();
+            console.log("Our card isn't up, vote for the lower ranked card");
+
+            var rows = $('.template_content')[0].children;
+            for (var i = 0; i < rows.length; i++) {
+                var rowTitle = $('.template_content')[0].children[i].children[1].children[0].title;
+
+                // First card is ranked higher, vote second card
+                if (rowTitle == title1) {
+                    console.log("voting for the second card");
+                    var button = $('#ideaCard2 .ideaDetails div .speuiSpriteButton a');
+                    button.click();
+                } 
+                
+                // Second card is ranked higher, vote first card
+                else if (rowTitle == title2) {
+                    console.log("voting for the first card");
+                    var button = $('#ideaCard1 .ideaDetails div .speuiSpriteButton a');
+                    button.click(); 
+               } 
+                
+                console.log(rowTitle);
+            }
+
+            // something went wront, move on
+            // var button = $('#ideaCard1 .ideaDetails div .speuiSpriteButton a');
+            // button.click(); 
         }
     }
 }
